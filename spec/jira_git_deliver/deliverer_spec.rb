fecaeab7ce3da90a32616a4491e1b17c284aac71
Ken Liu <ken.liu@gmail.com>
2016-07-23 19:26:12 -0400

    adds spec for deliverer

--- spec/jira_git_deliver/deliverer_spec.rb
+++ spec/jira_git_deliver/deliverer_spec.rb
@@ -1,7 +1,41 @@
 describe JiraGitDeliver::Deliverer do
-
   describe '#deliver' do
 
-  end
+    let(:jira) { instance_double('JiraGitDeliver::Jira') }
+    let(:git) { instance_double('JiraGitDeliver::Git') }
 
+    subject { JiraGitDeliver::Deliverer.new(jira, git) }
+
+    it 'delivers issues found by git' do
+      expect(jira).to receive(:finished_issues).with('test jql').and_return([OpenStruct.new(key: 'TEST-1')])
+      expect(git).to receive(:contains?).with('TEST-1').and_return(true)
+      expect(jira).to receive(:deliver).with('TEST-1', 'test')
+
+      subject.deliver('test jql', 'test')
+    end
+
+    it 'does nothing if no issues are found in git' do
+      expect(jira).to receive(:finished_issues).with('test jql').and_return([OpenStruct.new(key: 'TEST-1')])
+      expect(git).to receive(:contains?).with('TEST-1').and_return(false)
+      expect(jira).not_to receive(:deliver)
+
+      subject.deliver('test jql', 'test')
+    end
+
+    it 'does nothing if no issues are found in JIRA' do
+      expect(jira).to receive(:finished_issues).with('test jql').and_return([])
+      expect(git).not_to receive(:contains?)
+
+      subject.deliver('test jql', 'test')
+    end
+
+    it 'doesn\'t transition issues in dry run mode' do
+      expect(jira).to receive(:finished_issues).with('test jql').and_return([OpenStruct.new(key: 'TEST-1')])
+      expect(git).to receive(:contains?).with('TEST-1').and_return(true)
+      expect(jira).not_to receive(:deliver)
+      subject = JiraGitDeliver::Deliverer.new(jira, git, true) # dry_run = true
+
+      subject.deliver('test jql', 'test')
+    end
+  end
 end

ca056ce13019f33017ed356b3d9403ed1deb40f7
Ken Liu <ken.liu@gmail.com>
2016-07-23 20:50:58 -0400

    add git spec

--- spec/jira_git_deliver/git_spec.rb
+++ spec/jira_git_deliver/git_spec.rb
@@ -1,7 +1,14 @@
 describe JiraGitDeliver::Git do
 
+  let(:message) { "[Fixes #TEST-1]" }
+  let(:query) { "git log --grep='#{message}'" }
+  let(:result) { "Some git message" }
+  let(:git) { JiraGitDeliver::Git.new }
+
   describe '#contains?' do
-
+    it 'finds the message in the git log using system call' do
+      expect(git).to receive(:`).with(query) { result }
+      expect(git.contains?(message)).to eq(true)
+    end
   end
-
 end
